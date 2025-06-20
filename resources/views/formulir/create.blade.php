3d801a990c45cbad9f94c81e9c7599c281887333
Christsando <christsandoariel05@gmail.com>
2025-06-20 09:27:34 +0700

    change option value for jenis konsultasi

--- resources/views/formulir/create.blade.php
+++ resources/views/formulir/create.blade.php
@@ -29,10 +29,13 @@
                         <select id="jenisKonsultasi" name="jenisKonsultasi"
                             class="w-full px-3 py-2 border border-gray-300 rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent">
                             <option value="">Pilih jenis konsultasi</option>
-                            <option value="Konsultasi Personal">Konsultasi Personal</option>
-                            <option value="Konsultasi Bisnis">Konsultasi Bisnis</option>
-                            <option value="Konsultasi Karir">Konsultasi Karir</option>
-                            <option value="Konsultasi Pendidikan">Konsultasi Pendidikan</option>
+                            <option value="Konsultasi Legalitas Usaha">Konsultasi Legalitas Usaha</option>
+                            <option value="Konsultasi Branding & Desain Produk">Konsultasi Branding & Desain Produk</option>
+                            <option value="Konsultasi Penjualan & Distribusi">Konsultasi Penjualan & Distribusi</option>
+                            <option value="Konsultasi Teknologi untuk UMKM">Konsultasi Teknologi untuk UMKM</option>
+                            <option value="Konsultasi Pemasaran Digital">Konsultasi Pemasaran Digital</option>
+                            <option value="Konsultasi Keuangan UMKM">Konsultasi Keuangan UMKM</option>
+                            <option value="Konsultasi Perencanaan Bisnis">Konsultasi Perencanaan Bisnis</option>
                         </select>
                     </div>
 
